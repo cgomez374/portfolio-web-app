df26ed9fc039b611ddf31587e8a8023b38576375
Carlos Gomez <CGomez374@yahoo.com>
2021-09-19 19:36:48 -0700

    added project

--- src/Links.js
+++ src/Links.js
@@ -1,50 +1,56 @@
 export const links = [
-    {
-        type : 'personal projects',
-        name: 'countries API',
-        link : 'https://countries-api-app.netlify.app/',
-        img: 'countries-api.png'
-    },
-    {
-        type : 'minority programmers association',
-        name: 'consultancy page',
-        link : 'https://drive.google.com/file/d/13i4hvi6ukXTQHrKRuDb36UuMq4tVwCGj/view?usp=sharing',
-        img: 'mpa.png'
-    },
-    {
-        type : 'minority programmers association',
-        name: 'authentication page',
-        link : 'https://drive.google.com/file/d/1shAR9u1hLZmHiXysVbu6jE8sNwHa8Nsh/view?usp=sharing',
-        img: 'mpa.png'
-    },
-    {
-        type : 'minority programmers association',
-        name: 'social authentication page',
-        link : 'https://drive.google.com/file/d/1Y1J5Ai6mHIfSqK_QhLNUQpFTHD72Jh5m/view?usp=sharing',
-        img: 'mpa.png'
-    },
-    {
-        type : 'minority programmers association',
-        name: 'Learn More Skills Test',
-        link : 'https://skills-test-learn-page.vercel.app/',
-        img: 'mpa.png'
-    },
-    {
-        type : 'minority programmers association',
-        name: 'Incubator Page Skills Test',
-        link : 'https://incubator-page.vercel.app/',
-        img: 'mpa.png'
-    },
-    {
-        type : 'personal projects',
-        name: 'Hannesy Fitness',
-        link : 'https://cgomez374.github.io/Personal-Trainer/',
-        img: 'hannessy-fitness.png'
-    },
-    {
-        type : 'personal projects',
-        name: 'Movie API Search',
-        link : 'https://movie-api-search-app.netlify.app/',
-        img: 'movie-app.png'
-    }
-]
+  {
+    type: "personal projects",
+    name: "countries API",
+    link: "https://countries-api-app.netlify.app/",
+    img: "countries-api.png",
+  },
+  {
+    type: "minority programmers association",
+    name: "consultancy page",
+    link: "https://drive.google.com/file/d/13i4hvi6ukXTQHrKRuDb36UuMq4tVwCGj/view?usp=sharing",
+    img: "mpa.png",
+  },
+  {
+    type: "minority programmers association",
+    name: "authentication page",
+    link: "https://drive.google.com/file/d/1shAR9u1hLZmHiXysVbu6jE8sNwHa8Nsh/view?usp=sharing",
+    img: "mpa.png",
+  },
+  {
+    type: "minority programmers association",
+    name: "social authentication page",
+    link: "https://drive.google.com/file/d/1Y1J5Ai6mHIfSqK_QhLNUQpFTHD72Jh5m/view?usp=sharing",
+    img: "mpa.png",
+  },
+  {
+    type: "minority programmers association",
+    name: "Learn More Skills Test",
+    link: "https://skills-test-learn-page.vercel.app/",
+    img: "mpa.png",
+  },
+  {
+    type: "minority programmers association",
+    name: "Incubator Page Skills Test",
+    link: "https://incubator-page.vercel.app/",
+    img: "mpa.png",
+  },
+  {
+    type: "personal projects",
+    name: "Hannesy Fitness",
+    link: "https://cgomez374.github.io/Personal-Trainer/",
+    img: "hannessy-fitness.png",
+  },
+  {
+    type: "personal projects",
+    name: "manage landing page",
+    link: "https://cgomez374.github.io/manage-landing-page/",
+    img: "manage-landing-page.PNG",
+  },
+  {
+    type: "personal projects",
+    name: "Movie API Search",
+    link: "https://movie-api-search-app.netlify.app/",
+    img: "movie-app.png",
+  },
+];
