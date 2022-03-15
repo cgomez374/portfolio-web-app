2fb17768f924d62b0c74fb06bddd7e440380fe21
Carlos Gomez <CGomez374@yahoo.com>
2022-03-14 20:13:29 -0700

    added thumbnail and changed description for metadata

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import Styles from './ProjectsList.module.css';
+import React from "react";
+import Styles from "./ProjectsList.module.css";
 
-const ProjectList = ({projects}) => {
-    let title;
-    if(projects[0] !== undefined){
-        title = projects[0].type;
-    }
-    return (
-        <>
-            <h3>{title}</h3>
-            <div className={Styles.track}>
-                
-                {
-                    projects !== null &&
-                        projects.map(project => (
-                            <div className={Styles.projectCard}>
-                                <img src={project.img} alt="project image" />
-                                <h4><a href={project.link} target='_blank'>{project.name}</a></h4>
-                            </div>
-                        ))
-                }
+const ProjectList = ({ projects }) => {
+  let title;
+  if (projects[0] !== undefined) {
+    title = projects[0].type;
+  }
+  return (
+    <>
+      <h3>{title}</h3>
+      <div className={Styles.track}>
+        {projects !== null &&
+          projects.map((project, index) => (
+            <div className={Styles.projectCard} key={index}>
+              <img src={project.img} alt="project image" />
+              <h4>
+                <a href={project.link} target="_blank">
+                  {project.name}
+                </a>
+              </h4>
             </div>
-        </>
-    )
-}
+          ))}
+      </div>
+    </>
+  );
+};
 
-export default ProjectList
+export default ProjectList;
