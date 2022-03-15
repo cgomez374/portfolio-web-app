903f17d83ef18e7759fdea01e98cba35cd53c246
Carlos Gomez <CGomez374@yahoo.com>
2022-03-14 18:15:56 -0700

    added thumbnail

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import Styles from './Footer.module.css';
+import React from "react";
+import Styles from "./Footer.module.css";
 
 const Footer = () => {
+  const sendEmail = () => {
+    document.location = "mailto:cgomez37484@gmail.com";
+  };
+  return (
+    <footer className={Styles.footer} id={"contact"}>
+      <h2>Let's work together!</h2>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/cgomez374"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github fa-2x"></i>
+          </a>
+        </li>
+        <li onClick={sendEmail}>
+          <a href="#">
+            <i class="fas fa-envelope fa-2x"></i>
+          </a>
+        </li>
+        {/* <li>
+          <a
+            href="https://www.linkedin.com/in/carlos-gomez-666662ab/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-solid fa-circle-user fa-2x"></i>
+          </a>
+        </li> */}
+      </ul>
+    </footer>
+  );
+};
 
-    const sendEmail = () => {
-        document.location = "mailto:cgomez37484@gmail.com";
-    }
-    return (
-        <footer className={Styles.footer} id={'contact'}>
-            <h2>Let's work together!</h2>
-            <ul>
-                <li><a href="https://github.com/cgomez374" target='_blank' ><i class="fab fa-github fa-2x"></i></a></li>
-                <li onClick={sendEmail}><a href="#" ><i class="fas fa-envelope fa-2x"></i></a></li>
-                {/* <li><a href="#"><i class="fas fa-phone-square fa-2x"></i></a></li> */}
-            </ul>
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import Styles from './Header.module.css';
-import Nav from '../Nav/Nav';
+import React from "react";
+import Styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <header className={Styles.header} id={'home'} >
-            {/* NAVIGATION */}
-            {/* <Nav /> */}
-            {/* SHOWCASE */}
-            {/* <div className={Styles.showcase}>
-                <h1>hey i'm carlos</h1>
-                <h3>software engineer</h3>
-            </div> */}
-        </header>
-    )
-}
+  return (
+    <header className={Styles.header} id={"home"}>
+      {/* NAVIGATION */}
+      {/* <Nav /> */}
+      {/* SHOWCASE */}
+    </header>
+  );
+};
 
 export default Header;
