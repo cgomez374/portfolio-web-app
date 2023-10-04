f7e2499c4a688dd6c1dd0b82e75d95c2f3996ff3
Carlos Gomez <CGomez374@yahoo.com>
2023-10-03 19:51:44 -0700

    added technologies list to about me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,8 +16,52 @@ const About = () => {
           education to create impactful solutions in the field of software
           development.
         </p>
+        <ul className={Styles.techListContainer}>
+          <li>
+            <h3>front-end web technologies</h3>
+            <ul className={Styles.techList}>
+              <li>
+                <i class="fab fa-html5 fa-2x"></i>
+              </li>
+              <li>
+                <i class="fab fa-css3-alt fa-2x"></i>
+              </li>
+              <li>
+                <i class="fab fa-js fa-2x"></i>
+              </li>
+              <li>
+                <i class="fab fa-react fa-2x"></i>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>backend web technologies</h3>
+            <ul className={Styles.techList}>
+              <li>
+                <i class="fab fa-python fa-2x"></i>
+              </li>
+              <li>
+                <i class="fas fa-flask fa-2x"></i>
+              </li>
+              <li>
+                <i class="fas fa-database fa-2x"></i>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>other technologies</h3>
+            <ul className={Styles.techList}>
+              <li>
+                <i class="fab fa-git fa-2x"></i>
+              </li>
+              <li>
+                <i class="fab fa-aws fa-2x"></i>
+              </li>
+            </ul>
+          </li>
+        </ul>
         <div className={Styles.imgContainer}>
-          <img src={profile_pic} alt="me" />
+          <img src={profile_pic} alt="carlos gomez" />
         </div>
       </div>
     </div>
