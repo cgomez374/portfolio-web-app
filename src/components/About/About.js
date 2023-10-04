61597d22f12a71aa7708d672bf7134d1b76f19b1
Carlos Gomez <CGomez374@yahoo.com>
2023-10-03 17:45:39 -0700

    added some additional styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,10 +8,10 @@ const About = () => {
       <h2>meet me</h2>
       <div className={Styles.aboutInfo}>
         <p>
-          I'm Carlos, a first-generation college graduate with a passion for software
-          development and a background in customer-facing roles. Holding a
-          Master's degree in Computer Science, I am equipped with both technical
-          expertise and a deep appreciation for fostering meaningful
+          I'm Carlos, a first-generation college graduate with a passion for
+          software development and a background in customer-facing roles.
+          Holding a Master's degree in Computer Science, I am equipped with both
+          technical expertise and a deep appreciation for fostering meaningful
           interactions. I am excited to leverage my diverse experiences and
           education to create impactful solutions in the field of software
           development.
