e39228f65f25269eac73dae37bd25d3c9d8eff8b
Carlos Gomez <CGomez374@yahoo.com>
2023-10-03 17:07:47 -0700

    added an about me section

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Projects from './components/Projects/Projects';
-import Footer from './components/Footer/Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import Header from "./components/Header/Header";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <main>
-          <Route to='/'>
+          <Route to="/">
             <Header />
+            <About />
             <Projects />
             <Footer />
           </Route>
         </main>
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/About/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Styles from "./About.module.css";
+import profile_pic from "../../assets/profile_pic.jpg";
+
+const About = () => {
+  return (
+    <div className={Styles.about}>
+      <h2>meet me</h2>
+      <div className={Styles.aboutInfo}>
+        <p>
+          I am a first-generation college graduate with a passion for software
+          development and a background in customer-facing roles. Holding a
+          Master's degree in Computer Science, I am equipped with both technical
+          expertise and a deep appreciation for fostering meaningful
+          interactions. I am excited to leverage my diverse experiences and
+          education to create impactful solutions in the field of software
+          development.
+        </p>
+        <div className={Styles.imgContainer}>
+          <img src={profile_pic} alt="profile picture" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
