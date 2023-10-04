dce605a473a07cc9c667ac6c1035c757b10df823
Carlos Gomez <CGomez374@yahoo.com>
2023-10-03 21:02:18 -0700

    fixed send email bug

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,6 @@ import React from "react";
 import Styles from "./Footer.module.css";
 
 const Footer = () => {
-  const sendEmail = () => {
-    document.location = "mailto:cgomez37484@gmail.com";
-  };
   return <footer className={Styles.footer} id={"contact"}></footer>;
 };
 
