a9f79ef351f5f89a81b7404d6953e60adff6d5e2
Carlos Gomez <CGomez374@yahoo.com>
2023-10-09 16:56:51 -0700

    removed footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 import Icons from "./components/Icons/Icons";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
